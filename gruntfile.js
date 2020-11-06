37ed1052225bb7ad6029c0f823b0ee161b66b323
SteveNoyes <stevenmnoyes.af@gmail.com>
2020-11-06 15:48:34 -0800

    gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,50 +1,76 @@
 module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  const sass = require('node-sass');
+  require('load-grunt-tasks')(grunt);
+
+
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-
-    jshint: {
-      all: ['Gruntfile.js', '**/*.js'],
-    },
-
-    uglify: {
-      build: {
-        files: {
-          'dist/build.min.js': 'js/concat.js'
-        },
-      },
-    },
 
     concat: {
       dist: {
-        src: ['js/intro.js', 'js/project.js', 'js/main.js'],
-        dest: 'js/concat.js',
+        src: ['intro.js', 'project.js'],
+        dest: 'script.js',
       },
+    },
+
+    uglify: {
+       my_target: {
+         files: {
+           'script.min.js': ['script.js']
+         }
+       }
+     },
+
+
+    sass: {
+        options: {
+            implementation: sass,
+            sourceMap: true
+        },
+        dist: {
+            files: {
+                'style.css': 'style.scss'
+            }
+        }
     },
 
     cssmin: {
-      build: {
-        files: {
-          'dist/style.min.css': 'css/style.css'
-        },
-      },
+      target: {
+        files: [{
+          src: ['style.css'],
+          dest: 'style.min.css'
+        }]
+      }
     },
 
     watch: {
-      script: {
-        files: ['**/*.js', '**/*.css'],
-        tasks: ['concat', 'uglify', 'cssmin'],
+      options: {
+        spawn: false,
+        interrupt: true
       },
-    },
+      js: {
+        files: ['intro.js', 'project.js'],
+        tasks: ['concat', 'uglify']
+      },
 
+      sass: {
+        files: ['style.scss'],
+        tasks: ['sass']
+      },
+
+     css: {
+       files: ['style.css'],
+       tasks: ['cssmin']
+     }
+   }
 
   });
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
+  grunt.registerTask('default', ['watch'])
 
-  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
-
-};
+}
